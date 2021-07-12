ee4fa611a5f7568430c1c7723de144d8e12c7387
iwata-n <a.tokai.wain@gmail.com>
2021-07-12 16:36:15 +0900

    Fix impl

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,6 @@
-from discord.ext import commands
 import os
-import traceback
+import discord
 
-bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 client = discord.Client()
@@ -15,4 +13,4 @@ async def on_message(message):
     if message.content.startswith('$ぐえー'):
         await message.channel.send('ぐえー')
 
-bot.run(token)
+client.run(token)
